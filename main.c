5ed11405a7d64fe3aa9f66ce5daa3cc48ef657e0
JoelJohn08 <jmj6182@psu.edu>
2020-09-03 16:36:08 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main()
+{
+    char *temp= readline("Enter temperature in celsius: ");
+    double tempe = atof(temp);
+   double f;
+    f = ((tempe *9 / 5) + 32);
+    printf("%f° in Celsius is equivalent to %f° Fahrenheit. ", tempe, f);
+
+    return 0;
+}
